ace5bbffa5b0612aaad8bdca3f955f433df4af87
stoneyangxu <stoneyangxu@icloud.com>
2018-04-29 22:50:50 +0800

    add tests

--- src/about/views/About.jsx
+++ src/about/views/About.jsx
@@ -1,5 +1,10 @@
+// @flow
 import React from 'react';
 
-const About = () => <span>About</span>;
+export type Props = {
+  name: string
+};
+
+const About = name => <span>About {name}</span>;
 
 export default About;
